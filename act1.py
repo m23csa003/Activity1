ce17293f4b8ec911829a3912331d74e07426343d
m23csa003 <m23csa003@iitj.ac.in>
2024-03-27 00:01:08 +0530

    init commit

---
+++ act1.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# plot Sigmoid function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# plot ReLU function
+def relu(x):
+    return np.maximum(0, x)
+
+# plot Leaky ReLU function
+def leaky_relu(x):
+    return np.maximum(0.1*x, x)
+
+# plot tanh function
+def tanh(x):
+    return np.tanh(x)
+
+
+x = np.linspace(-10, 10, 100)
+
+y = sigmoid(x)
+plt.plot(x, y)
+plt.title('Sigmoid Function')
+plt.show()
+
+y = relu(x)
+plt.plot(x, y)
+plt.title('ReLU Function')
+plt.show()
+
+y = leaky_relu(x)
+plt.plot(x, y)
+plt.title('Leaky ReLU Function')
+plt.show()
+
+y = tanh(x)
+plt.plot(x, y)
+plt.title('tanh Function')
+plt.show()
+
